e8bb0f418883ca6892c588bb51a360be62a2a379
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-10-23 12:46:50 +0530

    Binary Tree

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,11 +5,15 @@ import { MAZES } from "../utils/constants";
 import { resetGrid } from "../utils/resetGrid";
 import type { MazeType } from "../utils/types";
 import { Select } from "./Select";
+import { runMazeAlgorithm } from "../utils/runMazeAlgorithm";
+import { useSpeed } from "../hooks/useSpeed";
 
 function Nav() {
   const [isDisabled, setIsDisabled] = useState(false);
-  const { maze, setMaze, grid } = usePathfinding();
+  const { maze, setMaze, grid, setGrid, setIsGraphVisualized } =
+    usePathfinding();
   const { startTile, endTile } = useTile();
+  const { speed } = useSpeed();
 
   const handleGenerateMaze = (maze: MazeType) => {
     if (maze === "NONE") {
@@ -19,7 +23,11 @@ function Nav() {
     }
 
     setMaze(maze);
-    setIsDisabled(false);
+    setIsDisabled(true);
+    runMazeAlgorithm({ maze, grid, startTile, endTile, setIsDisabled, speed });
+    const newGrid = grid.slice();
+    setGrid(newGrid);
+    setIsGraphVisualized(false);
   };
 
   return (
